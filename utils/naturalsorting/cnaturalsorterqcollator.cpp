ef3caa4ec0a6c8588417e8303200c7aff49b59db
treehugging-green-wolf <treehugging.green.wolf@google.com>
2020-09-25 12:08:43 +0300

    fix 1 file to support qt 5.8

--- utils/naturalsorting/cnaturalsorterqcollator.cpp
+++ utils/naturalsorting/cnaturalsorterqcollator.cpp
@@ -9,5 +9,5 @@ CNaturalSorterQCollator::CNaturalSorterQCollator()
 bool CNaturalSorterQCollator::compare(const QString & l, const QString & r, SortingOptions /*options*/) const
 {
 	// Fix for the new breaking changes in QCollator in Qt 5.14 - null strings are no longer a valid input
-	return _collator.compare(qToStringViewIgnoringNull(l), qToStringViewIgnoringNull(r)) < 0;
+	return _collator.compare(l.data(),l.size(),r.data(),r.size()) < 0;
 }
